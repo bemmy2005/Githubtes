bb15e43a7c34e6d0a3615862a928f81ea1c44bfc
BUGINGO <BUGINGO@DESKTOP-2NKSGIN>
2017-02-13 16:10:05 -0800

    May be it works, this is for testing

---
+++ Githubtest/src/cloud/lab/xmu/edu/cn/TestGithub.java
@@ -0,0 +1,13 @@
+package cloud.lab.xmu.edu.cn;
+
+public class TestGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git! you work fine!");
+	}
+
+}
